da5ffaa08d06538e33bcfe120c6421f7509534e6
JeffPadgett <JeffreyPadgett1@gmail.com>
2020-10-07 18:05:48 -0400

    Add project files.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCdemo.Models;
+using MVCDemo.Models;
+
+namespace MVCdemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Details(int id)
+        {
+
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmpID == id); 
+
+            return View(employee);
+        }
+
+        public ActionResult Index()
+        {
+
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employeeList = employeeContext.Employees.ToList();
+
+            return View(employeeList);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ViewResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "USA",
+                "ASIA",
+                "India",
+                "China"
+            };
+
+            return View();
+        }
+
+        public string GetDetails()
+        {
+            return "This is get details invoked";
+        }
+    }
+}
